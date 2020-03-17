ca3dde5cb5e99855370bf8391c60fb71bdd0ab90
sutthipong.lertmanorat@gmail.com <sutthipong.lertmanorat@gmail.com>
2020-03-17 21:27:18 +0000

    add 2nd example

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import Link from 'next/link';
 
 const Home = () => (
-        <Link href="/fileupload"><a>File upload</a></Link>
+    <div>
+        <div>
+            <Link href="/fileupload"><a>File upload</a></Link></div>
+        <div>
+            <Link href="/textpost"><a>Text post</a></Link></div>
+    </div>
+
 );
 
 export default Home;
